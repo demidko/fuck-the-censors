ebee7b7c0532d36307b067175af8502d82eb1b76
Daniel Demidko <demidko@farpost.com>
2022-11-16 02:40:07 +0500

    Доработан концепт

--- src/main/kotlin/online/censors/the/fuck/Replacer.kt
+++ src/main/kotlin/online/censors/the/fuck/Replacer.kt
@@ -14,7 +14,7 @@ class Replacer(vararg replace: Pair<Char, Char>) {
     val smallLetters = 'а'..'я'
     check(smallLetters.count() == 32)
     for (x in capitalLetters + smallLetters) {
-      check(replace(x) != x)
+      check(replace(x) != x) { "Replacement $x not found" }
     }
   }
 
