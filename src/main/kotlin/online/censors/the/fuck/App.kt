c711e8050d279b112fc310994e4035e9c4c462c2
Daniel Demidko <demidko@farpost.com>
2022-11-20 22:45:18 +0500

    Новые наработки

--- src/main/kotlin/online/censors/the/fuck/App.kt
+++ src/main/kotlin/online/censors/the/fuck/App.kt
@@ -12,18 +12,68 @@ import org.springframework.scheduling.annotation.EnableScheduling
 class App {
 
   @Bean
-  fun replacer(): Replacer {
-    return Replacer(
-      'А' to 'A',
-      'а' to 'a',
-      'б' to '?',
-    )
-  }
+  fun replacer() = Replacer(
+    'А' to 'A',
+    'а' to 'a',
+    'Б' to 'Ƃ',
+    'б' to 'ნ',
+    'В' to 'B',
+    'в' to 'ʙ',
+    'Г' to 'Γ',
+    'г' to 'ᴦ',
+    'Д' to 'Д',
+    'д' to 'д',
+    'Е' to 'E',
+    'е' to 'e',
+    'Ё' to 'E',
+    'ё' to 'e',
+    'Ж' to 'Ж',
+    'ж' to 'ж',
+    'З' to 'ვ',
+    'з' to 'ɜ',
+    'И' to 'ⵍ',
+    'и' to 'ᥢ',
+    'Й' to 'Ѝ',
+    'й' to 'ѝ',
+    'К' to 'K',
+    'к' to 'ĸ',
+    'Л' to 'Л',
+    'л' to 'ᴫ',
+    'М' to 'M',
+    'м' to 'ᴍ',
+    'Н' to 'H',
+    'н' to 'ʜ',
+    'О' to 'O',
+    'о' to 'o',
+    'П' to 'Π',
+    'п' to 'п',
+    'Р' to 'P',
+    'р' to 'p',
+    'С' to 'C',
+    'с' to 'c',
+    'Т' to 'T',
+    'т' to 'ᴛ',
+    'У' to 'У',
+    'у' to 'y',
+    'Ф' to 'Φ',
+    'ф' to 'ⱇ',
+    'Х' to 'X',
+    'х' to 'x',
+    'Ц' to 'Ц',
+    'ц' to 'ц',
+    'Ч' to 'Ⴗ',
+    'ч' to 'ɥ',
+    'Ш' to 'Ш',
+    'ш' to 'ꟺ',
+    'Щ' to 'щ',
+    'щ' to 'щ',
+    'Ъ' to 'Ⴆ',
+    'ъ' to 'ъ',
+  )
+
 
   @Bean
-  fun api(): Api {
-    return Api(replacer())
-  }
+  fun api() = Api(replacer())
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/online/censors/the/fuck/Replacer.kt
+++ src/main/kotlin/online/censors/the/fuck/Replacer.kt
@@ -1,32 +1,31 @@
 package online.censors.the.fuck
 
+import org.slf4j.LoggerFactory.getLogger
+
 class Replacer(vararg replace: Pair<Char, Char>) {
 
   private val replacements = replace.toMap()
 
   init {
-    // todo checkCyrillic()
-    // todo checkEnglish()
+    checkAlphabet("Russian", 32, 'А'..'Я', 'а'..'я')
+    checkAlphabet("English", 26, 'A'..'Z', 'a'..'z')
   }
 
-  private fun checkEnglish() {
-    checkAlphabet('A'..'Z', 26)
-    checkAlphabet('a'..'z', 26)
-  }
-
-  private fun checkRussian() {
-    checkAlphabet('А'..'Я', 32)
-    checkAlphabet('а'..'я', 32)
-  }
-
-  private fun checkAlphabet(alphabet: CharRange, count: Int) {
-    check(alphabet.count() == count)
-    for (letter in alphabet) {
-      check(replace(letter) != letter) { "Replacement $letter not found" }
+  private fun checkAlphabet(name: String, count: Int, vararg letters: CharRange) {
+    check(letters.count() == count)
+    val log = getLogger(javaClass)
+    for (l in letters.asList().flatten()) {
+      if (replaced(l) == l) {
+        log.warn("Replacement '$l' ($name) not found")
+      }
     }
   }
 
-  private fun replace(c: Char) = replacements[c] ?: c
+  private fun replaced(c: Char) = replacements[c] ?: c
 
-  fun obfuscate(text: String) = text.map(::replace).joinToString("")
+  fun obfuscate(text: String) = buildString {
+    for (l in text) {
+      append(replaced(l))
+    }
+  }
 }
