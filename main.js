894ecd0d8e433bcec3a5d6bae8c247542aac48db
Daniel Demidko <demidko@farpost.com>
2022-12-24 15:18:58 +0500

    Only one notification at a time allowed

--- main.js
+++ main.js
@@ -2,16 +2,23 @@
 
 import {obfuscate} from "./replacer.js";
 
+let lastNotificationTimestamp = 0
+
 window.obfuscateTextarea = async function (textarea) {
   let resultContainer = document.getElementById("result-container")
   resultContainer.hidden = false
   resultContainer.value = obfuscate(textarea.value)
   resultContainer.setSelectionRange(textarea.selectionStart, textarea.selectionEnd)
   await navigator.clipboard.writeText(resultContainer.value)
-  $(".ui.toast").toast("destroy")
-  $.toast({
-    class: "success",
-    showIcon: "copy",
-    message: "Text copied to clipboard"
-  });
+  let displayTimeMs = 1000
+  let lastNotificationAgo = Date.now() - lastNotificationTimestamp
+  if (lastNotificationAgo > displayTimeMs) {
+    $.toast({
+      displayTime: displayTimeMs,
+      class: "success",
+      showIcon: "copy",
+      message: "Text copied to clipboard"
+    });
+    lastNotificationTimestamp = Date.now()
+  }
 }
